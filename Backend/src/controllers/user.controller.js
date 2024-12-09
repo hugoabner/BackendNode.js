d0ee1d2339cabb5e39c373be29e68088e7773e6b
Hugo <hugopalominogalindo974@gmail.com>
2024-12-09 14:01:53 -0500

    se temino la actividad con los endpoints de auth

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -19,7 +19,6 @@ export const authUser = asyncHandler(async (req, res) => {
 	}
 });
 
-
 /**@Function para registrar usuario **/
 export const registerUser = asyncHandler( async(req, res) => {
 	const { name, email, password } = req.body; 
@@ -60,14 +59,31 @@ export const logoutUser = asyncHandler(async(req, res) => {
 
 /**@Funtion para obtener informacion del usuario **/
 export const getUserProfile = asyncHandler(async(req, res)=> {
-	res.status(200).json({
-		message: 'desde la funcion get user profile'
-	})
+	const user = {
+		_id: req.user._id,
+		name: req.user.name,
+		email: req.user.email
+	}
+	res.status(200).json(user);
 });
 
 /**@Function para actualizar el perfil del usuario**/
 export const updateUserProfile = asyncHandler(async(req, res) => {
-	res.status(200).json({
-		message: 'desde la funcion update user profile'
-	})
+	const user = await User.findById(req.user._id);
+	if (user) {
+		user.name = req.body.name || user.name;
+		user.email = req.body.email || user.email;
+		if (req.body.password) {
+			user.password = req.body.password;
+		}
+		const updateUser = await user.save();
+		res.status(200).json({
+			_id: updateUser._id,
+			name: updateUser.name,
+			email: updateUser.email
+		})
+	} else {
+		res.status(404);
+		throw new Error('Usuario no encontrado');
+	} 
 });
